8f35ebc4fe2c2d9d887378379ddace5b9c599432
zeyad2 <zeyadsattar.me@gmail.com>
2025-06-03 02:11:13 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => (
+  <main className="relative">
+
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding">Services </section>
+    <section className="padding-x padding-y">SpecialOffers</section>
+  </main>
+);
+
+export default App;
